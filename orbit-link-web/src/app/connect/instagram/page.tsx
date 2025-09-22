e484d00383091f5fd8b78845c99d1f790baf4855
jonghwi <jonghwi@hecto.co.kr>
2025-09-22 16:11:58 +0900

    instagram oauth url 변경

--- orbit-link-web/src/app/connect/instagram/page.tsx
+++ orbit-link-web/src/app/connect/instagram/page.tsx
@@ -3,11 +3,13 @@
 export default function LoginPage() {
   const clientId = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID!;
   const redirectUri = process.env.NEXT_PUBLIC_FACEBOOK_REDIRECT_URI!;
-  const scopes = "pages_show_list,instagram_basic,instagram_manage_messages"; // 필요한 권한
+  // 필요한 권한
+  const scopes =
+    "instagram_business_basic,instagram_business_content_publish,instagram_business_manage_messages,instagram_business_manage_comments";
 
-  const loginUrl = `https://www.facebook.com/v21.0/dialog/oauth?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+  const loginUrl = `https://www.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
     redirectUri
-  )}&scope=${encodeURIComponent(scopes)}`;
+  )}&response_type=code&scope=${encodeURIComponent(scopes)}`;
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh]">
